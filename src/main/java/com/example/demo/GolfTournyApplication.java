517032cb1af09706181026abbdb27fd17dcb133e
Tyler Wall <Tyler.wall@keyin.com>
2025-03-21 18:01:16 -0230

    initial commit take 2. Other project was too messy and I got lost

---
+++ src/main/java/com/example/demo/GolfTournyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfTournyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfTournyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/tournament/TournamentRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo.tournament;
+
+public class TournamentRepository {
+}
